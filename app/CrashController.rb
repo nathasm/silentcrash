0e02e31978d31c7dbd4945343b9a5dd081f1cfbe
Nathan Smith <nsmith@atdesk.com>
2012-06-01 08:56:02 -0400

    Add non-dispatched queue method and button

--- app/CrashController.rb
+++ app/CrashController.rb
@@ -5,10 +5,15 @@ class CrashController < UIViewController
       self, action:'crash_button', forControlEvents:UIControlEventTouchUpInside)
     view.addSubview(@crashButton)
 
-    @goodButton = add_button("Good Button", 300, 30)
+    @goodButton = add_button("Good Button", 200, 30)
     @goodButton.addTarget(
       self, action:'good_button', forControlEvents:UIControlEventTouchUpInside)
     view.addSubview(@goodButton)
+
+    @noDispatchButton = add_button("No Dispatch Queue", 300, 30)
+    @noDispatchButton.addTarget(
+      self, action:'no_dispatch_button', forControlEvents:UIControlEventTouchUpInside)
+    view.addSubview(@noDispatchButton)
   end
 
   def add_button(text, y, height)
@@ -49,6 +54,15 @@ class CrashController < UIViewController
       end
       Dispatch::Queue.main.sync { print_results(data) }
     end
+  end
+
+  def no_dispatch_button
+    p "No Dispatch Queue Button pushed"
+
+    url = "http://www.google.com"
+    data = NSData.alloc.initWithContentsOfURL(
+      NSURL.URLWithString(url))
+    print_results(data) 
   end
 
   def print_results(data)
